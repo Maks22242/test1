994f7a4ba7e200c05dea78ffc5337fd44dc675b0
Maks22242 <makcum.petrenko@mail.ru>
2023-04-29 09:09:15 +0300

    patch2

--- report.md
+++ report.md
@@ -1,2 +1,4 @@
 1. added file
 2. changed code style
+3. merged
+4. patch2
--- t.cpp
+++ t.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
 
 
 int main() {
-    std::cout<<"hhh"l
+    string a;
+    cin>>a;
+    std::cout<<"hhh"<<a;
 }
